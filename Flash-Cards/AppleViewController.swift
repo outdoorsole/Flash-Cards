7281534be92ccb939f86e5ded53ea2a446b3ca08
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-11-20 22:43:01 -0800

    Create ind VC files for each view

---
+++ Flash-Cards/AppleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AppleViewController.swift
+//  Flash-Cards
+//
+//  Created by Maribel Montejano on 11/20/17.
+//  Copyright © 2017 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class AppleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
