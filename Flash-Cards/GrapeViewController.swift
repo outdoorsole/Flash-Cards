c58af38050bba1c790d9344d6be47c58f2f1bd58
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-11-20 23:49:45 -0800

    Add backButton action (to dismiss VC)

--- Flash-Cards/GrapeViewController.swift
+++ Flash-Cards/GrapeViewController.swift
@@ -15,21 +15,9 @@ class GrapeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
